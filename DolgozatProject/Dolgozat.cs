ba9e3794cbf689712889822bbeadfdde4ff623ae
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-12-09 15:17:53 +0100

    PontFelvesz(), MindenkiMegirta()

--- DolgozatProject/Dolgozat.cs
+++ DolgozatProject/Dolgozat.cs
@@ -2,6 +2,22 @@
 {
 	public class Dolgozat
 	{
+		private List<int> pontok;
 
+		public Dolgozat()
+		{
+			pontok = new List<int>();
+		}
+
+		public void PontFelvesz(int x)
+		{
+			if (x < -1 || x > 100) throw new ArgumentException("A pontnak -1 és 100 között kell lennie", nameof(x));
+			pontok.Add(x);
+		}
+
+		public bool MindenkiMegirta()
+		{
+			return !pontok.Contains(-1);
+		}
 	}
 }
--- TestDolgozatProject/DolgozatTest.cs
+++ TestDolgozatProject/DolgozatTest.cs
@@ -1,16 +1,59 @@
+using DolgozatProject;
+
 namespace TestDolgozatProject
 {
 	public class Tests
 	{
+		public Dolgozat dolgozat;
+
 		[SetUp]
 		public void Setup()
 		{
+			dolgozat = new Dolgozat();
 		}
 
 		[Test]
-		public void Test1()
+		public void PontFelvesz_HelyesAdat_NemDobArgumentExceptiont()
 		{
-			Assert.Pass();
+			Assert.DoesNotThrow(() => dolgozat.PontFelvesz(10));
 		}
+
+		[Test]
+		public void PontFelvesz_TulNagyAdat_ArgumentExceptiontDob()
+		{
+			Assert.Throws<ArgumentException>(() => dolgozat.PontFelvesz(101));
+		}
+
+		[Test]
+		public void PontFelvesz_TulKicsiAdat_ArgumentExceptiontDob()
+		{
+			Assert.Throws<ArgumentException>(() => dolgozat.PontFelvesz(-2));
+		}
+
+		[Test]
+		public void MindenkiMegirta_MindenkiIgen_HelyesenMukodik()
+		{
+			dolgozat.PontFelvesz(10);
+			dolgozat.PontFelvesz(20);
+
+			Assert.IsTrue(dolgozat.MindenkiMegirta());
+		}
+
+		[Test]
+		public void MindenkiMegirta_NemMindenki_HelyesenMukodik()
+		{
+			dolgozat.PontFelvesz(10);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(20);
+
+			Assert.IsFalse(dolgozat.MindenkiMegirta());
+		}
+
+		[Test]
+		public void MindenkiMegirta_UresLista_HelyesenMukodik()
+		{
+			Assert.IsTrue(dolgozat.MindenkiMegirta());
+		}
+
 	}
 }
